0186eb25965feaf8dd9bff27c09bb1128ba9983a
Sarah <sarah.boerger@gmail.com>
2020-10-06 15:40:45 +0200

    All files - First upload

---
+++ BrainTraffic-Files/Wordpress/footer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package BrainTraffic
+ */
+ 
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<p>© 2020 Brain Traffic</p>
+			<ul>
+				<li><a href="#"> Confab Conference </a></li>
+				<li><a href="#"> Button Conferenceb </a></li>
+				<li><a href="#"> Content Strategy Podcast </a></li>
+				<li><a href="#"> Contact Us </a></li>
+				<li><a href="#"> Newsletter </a></li>
+				<li><a href="#"> Twitter </a></li>
+				<li><a href="#"> LinkedIn </a></li>
+			</ul>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+<!---font awesome kode--> 
+<script src="https://kit.fontawesome.com/5e963cd551.js" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
+
